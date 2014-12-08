0ace75f5b790728696c3a2b1f9ea393e0792b888
Mike Wilkes <mikezx6r@gmail.com>
2014-12-07 22:50:50 -0500

    Enable better logging on command-line when running tests

--- src/test/java/net/thucydides/jbehave/ASampleBehaviorForUatOnly.java
+++
@@ -1,9 +0,0 @@
-package net.thucydides.jbehave;
-
-import net.thucydides.core.webdriver.Configuration;
-
-public class ASampleBehaviorForUatOnly extends ThucydidesJUnitStory {
-    public ASampleBehaviorForUatOnly(Configuration configuration) {
-        super(configuration);
-    }
-}
