05511080046f0817486384bb6ab62eb21034a8f5
YamStranger <YamStranger@bk.ru>
2016-02-22 15:56:40 +0200

    feat: updated managed tag processing Now it is possible to mark as manages all scenarious in story file ``` Narrative: ...
    Meta:
    @manual
    
    Scenario: A scenario that works and should me manual
    Given I have an implemented JBehave scenario
    ...
    
    Scenario: A scenario that works and should me manual too
    Given I have an implemented JBehave scenario
    ...

---
+++ src/test/java/net/serenitybdd/jbehave/WhenRunningJBehaveStoriesManualTags.java
@@ -0,0 +1,72 @@
+package net.serenitybdd.jbehave;
+
+import net.thucydides.core.model.TestOutcome;
+import net.thucydides.core.model.TestResult;
+import net.thucydides.core.model.TestTag;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+
+import static net.thucydides.core.reports.matchers.TestOutcomeMatchers.havingTag;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class WhenRunningJBehaveStoriesManualTags extends AbstractJBehaveStory {
+
+    @Test
+    public void a_test_should_be_marked_as_manual_when_manual_tag_provided() throws Throwable {
+
+        // Given
+        SerenityStories story = newStory("aBehaviorWithManualScenario.story");
+
+        // When
+        run(story);
+
+        // Then
+        List<TestOutcome> outcomes = loadTestOutcomes();
+        assertThat(outcomes.get(0), havingTag(TestTag.withName("Manual").andType("External Tests")));
+        assertThat(outcomes.get(0).isManual(), equalTo(true));
+        assertThat(outcomes.get(0).getResult(), is(TestResult.PENDING));
+    }
+
+    @Test
+    public void steps_should_be_marked_manual_if_tag_provided_at_story_level() throws Throwable {
+
+        // Given
+        SerenityStories story = newStory("aBehaviorWithManualStory.story");
+
+        // When
+        run(story);
+
+        // Then
+        List<TestOutcome> outcomes = loadTestOutcomes();
+        assertThat(outcomes.get(0), havingTag(TestTag.withName("Manual").andType("External Tests")));
+        assertThat(outcomes.get(0).isManual(), equalTo(true));
+        assertThat(outcomes.get(0).getResult(), is(TestResult.PENDING));
+        assertThat(outcomes.get(1), havingTag(TestTag.withName("Manual").andType("External Tests")));
+        assertThat(outcomes.get(1).isManual(), equalTo(true));
+        assertThat(outcomes.get(1).getResult(), is(TestResult.PENDING));
+
+    }
+
+    @Test
+    public void scenario_manual_tags_should_not_be_shared_between_scenarios() throws Throwable {
+
+        // Given
+        SerenityStories story = newStory("aBehaviorWithManualAndNotManualScenario.story");
+
+        // When
+        run(story);
+
+        // Then
+        List<TestOutcome> outcomes = loadTestOutcomes();
+        assertThat(outcomes.get(0), havingTag(TestTag.withName("Manual").andType("External Tests")));
+        assertThat(outcomes.get(0).isManual(), equalTo(true));
+        assertThat(outcomes.get(0).getResult(), is(TestResult.PENDING));
+
+        assertThat(outcomes.get(1).getResult(), is(TestResult.SUCCESS));
+        assertThat(outcomes.get(1).isManual(), equalTo(false));
+        assertThat(outcomes.get(1).getTags().size(), is(1));
+    }
+}
